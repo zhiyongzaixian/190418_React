3437c1253ddba719dd2f64160c6ef71108749e71
zhiyongzaixian <13852292775@163.com>
2019-08-05 17:05:53 +0800

    'first'

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+
+
+// 定义组件
+// 暴露组件
+export default class App extends Component {
+  constructor(props){
+    super(props);
+  }
+  
+  render(){
+    return (
+      <div>
+        <h1>App应用</h1>
+      </div>
+    )
+  }
+}
+
+
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './App'
+
+// 渲染组件到页面容器
+ReactDOM.render(<App />,  document.getElementById('root'))
